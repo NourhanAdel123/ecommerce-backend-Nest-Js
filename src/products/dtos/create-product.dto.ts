e938faa3166e46266dd67f718feff331b85dab3d
Nourhan Adel <143334844+Nora-Shalaby@users.noreply.github.com>
2026-09-08 22:36:46 +0300

    feat: add global validation and DTO validation for products

--- src/products/dtos/create-product.dto.ts
+++ src/products/dtos/create-product.dto.ts
@@ -1,4 +1,11 @@
+import { IsString, IsNumber, IsNotEmpty } from 'class-validator';
+
 export class CreateProductDto {
+  @IsString()
+  @IsNotEmpty()
   name: string;
+
+  @IsNumber()
+  @IsNotEmpty()
   price: number;
 }
--- src/products/dtos/update-product.dto.ts
+++ src/products/dtos/update-product.dto.ts
@@ -1,4 +1,13 @@
+import { IsString, IsNumber, IsNotEmpty, IsOptional } from 'class-validator';
+
 export class updateProductDto {
+  @IsString()
+  @IsNotEmpty()
+  @IsOptional()
   name?: string;
+
+  @IsNumber()
+  @IsNotEmpty()
+  @IsOptional()
   price?: number;
 }
